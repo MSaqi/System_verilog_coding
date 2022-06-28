5796e921401da09ce0f5c1be829d133891c4d4c5
MSaqi <99978864+MSaqi@users.noreply.github.com>
2022-06-28 18:53:41 +0500

    Duplicate file

--- type_def.sv
+++
@@ -1,9 +0,0 @@
-package typedefs;
-
-typedef enum 
-{INST_ADDR=0,INST_FETCH,INST_LOAD,IDLE,OP_ADDR,OP_FETCH,ALU_OP,STORE}states_t;
-
-typedef enum logic [2:0]
-{HLT,SKZ,ADD,AND,XOR,LDA,STO,JMP}opcode_t;
-endpackage
-
